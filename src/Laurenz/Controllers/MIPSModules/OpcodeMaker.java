49ce24caeccd7bbd5a9080604a235c3a88b9ff67
pmvhernandez <pmvhernandez@gmail.com>
2016-12-12 09:31:39 +0800

    Opcode + Parser
    Completed OpcodeMaker and added/made some changes to Parser

--- src/Laurenz/Controllers/MIPSModules/OpcodeMaker.java
+++ src/Laurenz/Controllers/MIPSModules/OpcodeMaker.java
@@ -11,115 +11,166 @@ import java.util.HashMap;
 public class OpcodeMaker
 {
 
-	ArrayList<Instruction> instructions;
+    ArrayList<Instruction> instructions;
 
 
 
-	public OpcodeMaker()
-	{
+    public OpcodeMaker()
+    {
 
-	}
+    }
 
-	public static void opcodeMap()
-	{
-		HashMap<String, Integer> opcodeMap = new HashMap<>();
+    public static void opcodeMap()
+    {
+        HashMap<String, Integer> opcodeMap = new HashMap<>();
 
- 	}
+    }
 
-	public ArrayList<Instruction> populateInstructionOpcodes(ArrayList<Instruction> instructions)
-	{
-		ArrayList<Instruction> updatedInstructions = instructions;
-		int opcodeValue, funcCodeValue;
-		String command = "", opcode = "";
-		Instruction instruction;
+    public ArrayList<Instruction> populateInstructionOpcodes(ArrayList<Instruction> instructions)
+    {
+        ArrayList<Instruction> updatedInstructions = instructions;
+        int opcodeValue, funcCodeValue;
+        String command = "", opcode = "";
+        Instruction instruction;
 
-		for (int i = 0; i < instructions.size(); i++)
+        for (int i = 0; i < instructions.size(); i++)
+        {
+            instruction = updatedInstructions.get( i );
+            opcode = setHexOpcode( i, instruction );
+            instruction.setOpcode( opcode );
+            updatedInstructions.set(i, instruction);
+        }
+
+        return updatedInstructions;
+    }
+
+//  "OR", "DSUBU", "SLT", "NOP",
+//  "BNE", "LD", "SD", "SW", "DADDIU",
+//  "J"
+
+    public String setHexOpcode(int i, Instruction instruction)
+    {
+        String opcode, command, rs = "" ,rd = "", rt = "", imm = "", hexOpcode = "", func = "";
+        int opcodeDecimal = 0;
+
+        command = instruction.getCommand();
+
+        /* list down all opcodes */
+        /*
+        * please follow the format below in DADDIU
+        * */
+        if( command.equalsIgnoreCase("DADDIU") )
+        {
+            opcodeDecimal = 25; // decimal equivalent of the opcode
+            opcode  = StringUtils.leftPad( Integer.toBinaryString(opcodeDecimal), 6, "0");
+            rs      = getRsToBinary( instruction.getRs(), 5 );
+            rt      = getRsToBinary( instruction.getRt(), 5);
+            rd      = "";
+            imm     = convertHexToBinary( instruction.getImm(), 16);
+
+            // hexOpcode = opcode + rs + rd + imm;
+
+        }
+        else if ( command.equalsIgnoreCase("DSUBU") )
+        {
+            opcode  = "000000";
+            rs      = getRsToBinary( instruction.getRs(), 5);
+            rt      = getRsToBinary( instruction.getRt(), 5);
+            rd      = getRsToBinary( instruction.getRd(), 5);
+            imm     = "00000";
+            func    = "101111";
+        }
+        else if( command.equalsIgnoreCase("OR") )
 		{
-			instruction = updatedInstructions.get( i );
-			opcode = setHexOpcode( i, instruction );
-			instruction.setOpcode( opcode );
-			updatedInstructions.set(i, instruction);
-		}
-
-		return updatedInstructions;
-	}
-
-//	"OR", "DSUBU", "SLT", "NOP",
-//			"BNE", "LD", "SD", "SW", "DADDIU",
-//			"J"
-
-	public String setHexOpcode(int i, Instruction instruction)
-	{
-		String opcode, command, rs = "" ,rd = "", rt = "", imm = "", hexOpcode = "", func = "";
-		int opcodeDecimal = 0;
-
-		command = instruction.getCommand();
-
-		/* list down all opcodes */
-		/*
-		* please follow the format below in DADDIU
-		* */
-		if( command.equalsIgnoreCase("DADDIU") )
-		{
-			opcodeDecimal = 25; // decimal equivalent of the opcode
-			opcode 	= StringUtils.leftPad( Integer.toBinaryString(opcodeDecimal), 6, "0");
-			rs 		= getRsToBinary( instruction.getRs(), 5 );
-			rt 		= "";
-			rd 		= getRsToBinary( instruction.getRd(), 5);
-			imm		= convertHexToBinary( instruction.getImm(), 16);
-
-			// hexOpcode = opcode + rs + rd + imm;
-
-		}
-		else if ( command.equalsIgnoreCase("DSUBU") )
-		{
-			opcode	= "000000";
+			opcode 	= "000000";
 			rs 		= getRsToBinary( instruction.getRs(), 5);
 			rt 		= getRsToBinary( instruction.getRt(), 5);
 			rd 		= getRsToBinary( instruction.getRd(), 5);
-			imm 	= "00000";
-			func 	= "101111";
+			imm     = "00000";
+			func	= "100101";
 		}
-		else
+		else if( command.equalsIgnoreCase("SLT") )
 		{
-			hexOpcode = "NaN";
-			opcode = "";
-			rs = "";
-			rd = "";
-			imm = "";
+			opcode 	= "000000";
+			rs 		= getRsToBinary( instruction.getRs(), 5);
+			rt 		= getRsToBinary( instruction.getRt(), 5);
+			rd 		= getRsToBinary( instruction.getRd(), 5);
+			imm     = "00000";
+			func	= "101010";
 		}
+		else if( command.equalsIgnoreCase("LD") )
+		{
+			opcodeDecimal = 55;
+			opcode 	= StringUtils.leftPad( Integer.toBinaryString(opcodeDecimal), 6, "0");
+			rs 	    = getRsToBinary( instruction.getBase(), 5);
+			rt 		= getRsToBinary( instruction.getRt(), 5);
+			imm  	= convertHexToBinary( instruction.getOffset(), 16);
+		}
+		else if( command.equalsIgnoreCase("SD") )
+		{
+			opcodeDecimal = 63;
+			opcode 	= StringUtils.leftPad( Integer.toBinaryString(opcodeDecimal), 6, "0");
+			rs 	    = getRsToBinary( instruction.getBase(), 5);
+			rt 		= getRsToBinary( instruction.getRt(), 5);
+			imm	    = convertHexToBinary( instruction.getOffset(), 16);
+		}
+		else if( command.equalsIgnoreCase("BNE") )
+		{
+			opcodeDecimal = 5;
+			opcode 	= StringUtils.leftPad( Integer.toBinaryString(opcodeDecimal), 6, "0");
+			rs 		= getRsToBinary( instruction.getRs(), 5);
+			rt 		= getRsToBinary( instruction.getRt(), 5);
+			imm  	= convertHexToBinary( instruction.getImm(), 16);
+		}
+		else if( command.equalsIgnoreCase("J") )
+		{
+			opcodeDecimal = 2;
+			opcode 	= StringUtils.leftPad( Integer.toBinaryString(opcodeDecimal), 6, "0");
+			//instr_index
+			imm  	= convertHexToBinary( instruction.getOffset(), 26);
+		}
+		else if( command.equalsIgnoreCase("NOP") )
+		{
+			opcodeDecimal = 0;
+			opcode 	= StringUtils.leftPad( Integer.toBinaryString(opcodeDecimal), 32, "0");
+		}
+        else
+        {
+            hexOpcode = "NaN";
+            opcode = "";
+        }
 
-		hexOpcode = opcode + rs + rt + rd + imm + func;
-		Print.ln("OPCODE in Binary: " + opcode + " " + rs + " " + rt + " " + rd + " " + imm + " " +  func);
-		hexOpcode = convertBinaryToHex(hexOpcode);
-		hexOpcode = hexOpcode.toUpperCase();
-		Print.ln("OPCODE in Hex: " + hexOpcode);
-		return hexOpcode;
-	}
+        hexOpcode = opcode + rs + rt + rd + imm + func;
+        Print.ln("OPCODE in Binary: " + opcode + " " + rs + " " + rt + " " + rd + " " + imm + " " +  func);
+        hexOpcode = convertBinaryToHex(hexOpcode);
+        hexOpcode = hexOpcode.toUpperCase();
+        Print.ln("OPCODE in Hex: " + hexOpcode);
+        return hexOpcode;
+    }
 
-	private String getRsToBinary(String R, int length)
-	{
-		String reg  = R.substring(1);
-		String temp = Integer.toBinaryString( Integer.parseInt(reg) );
-		return StringUtils.leftPad(temp, length, "0");
-	}
+    private String getRsToBinary(String R, int length)
+    {
+        String reg  = R.substring(1);
+        String temp = Integer.toBinaryString( Integer.parseInt(reg) );
+        return StringUtils.leftPad(temp, length, "0");
+    }
 
-	public String convertBinaryToHex(String opBinary)
-	{
-		String opHex;
-		BigInteger bigInt;
-		bigInt 	= new BigInteger(opBinary, 2);
-		opHex 	= bigInt.toString(16);
-		opHex  	= StringUtils.leftPad(opHex, 8, "0");
+    public String convertBinaryToHex(String opBinary)
+    {
+        String opHex;
+        BigInteger bigInt;
+        bigInt  = new BigInteger(opBinary, 2);
+        opHex   = bigInt.toString(16);
+        opHex   = StringUtils.leftPad(opHex, 8, "0");
 
-		return opHex;
-	}
+        return opHex;
+    }
 
-	public String convertHexToBinary(String hex, int length)
-	{
-		String result;
-		result = Integer.toBinaryString( Integer.parseInt(hex, 16) );
-		result = StringUtils.leftPad(result, length, "0");
-		return result;
-	}
+    public String convertHexToBinary(String hex, int length)
+    {
+        String result;
+        result = Integer.toBinaryString( Integer.parseInt(hex, 16) );
+        result = StringUtils.leftPad(result, length, "0");
+        return result;
+    }
 }
--- src/Laurenz/Controllers/MIPSModules/Parser.java
+++ src/Laurenz/Controllers/MIPSModules/Parser.java
@@ -11,97 +11,173 @@ import java.util.Scanner;
 public class Parser
 {
 
-	public static void main(String[] args)
-	{
-		String text = "DADDIU R1, R0, #0009 \nDADDIU R2, R0, #1111";
-		Parser p = new Parser();
+    public static void main(String[] args)
+    {
+        String text = "DADDIU R1, R1, #4444 \nDADDIU R2, R0, #1111";
+        Parser p = new Parser();
 
-		p.parse(text);
-	}
-	public Parser()
-	{
+        p.parse(text);
+    }
+    public Parser()
+    {
 
-	}
+    }
 
-	public ArrayList<Instruction> parse(String text)
-	{
-		/* Declare variables */
-		ArrayList<Instruction> instructions;
-		String singleLine, command;
-		String[] lines;
-		/* Instruction set for the group */
-		String[] instructionsSetB =
-		{
-				"OR", "DSUBU", "SLT", "NOP",
-				"BNE", "LD", "SD", "SW", "DADDIU",
-				"J"
-		};
-		/* values */
-		instructions = new ArrayList<>();
-		text = text.replaceAll("(?m)^[ \t]*\r?\n", "");
-		lines 		 = text.split("\\r?\\n");
+    public ArrayList<Instruction> parse(String text)
+    {
+        /* Declare variables */
+        ArrayList<Instruction> instructions;
+        String singleLine, command;
+        String[] lines;
+        /* Instruction set for the group */
+        String[] instructionsSetB =
+        {
+                "OR", "DSUBU", "SLT", "NOP",
+                "BNE", "LD", "SD", "DADDIU",
+                "J"
+        };
+        /* values */
+        instructions = new ArrayList<>();
+        text = text.replaceAll("(?m)^[ \t]*\r?\n", "");
+        lines        = text.split("\\r?\\n");
 
-		for(int i = 0; i < lines.length; i++ )
-		{
-			String[] registers = new String[3];
-			singleLine = lines[i];
-			Scanner scanner = new Scanner(singleLine);
-			Print.ln(singleLine);
+        for(int i = 0; i < lines.length; i++ )
+        {
+            String[] registers = new String[3];
+            singleLine = lines[i];
+            Scanner scanner = new Scanner(singleLine);
+            Print.ln(singleLine);
 
-			String[] temp = singleLine.split(" ");
-			command = temp[0];
-			registers[0] = temp[1].split(",")[0];
-			registers[1] = temp[2].split(",")[0];
-			registers[2] = temp[3];
-			Print.ln("command: " + command );
-			Print.ln("rd: " + registers[0]);
-			Print.ln("rs: " + registers[1]);
-			Print.ln("imm: " + registers[2]);
+            String[] temp = singleLine.split(" ");
+            command = temp[0];
+            if(temp.length == 1 && command.equalsIgnoreCase("NOP"))
+            {
+                Print.ln("command: " + command );
+            }
+            else if(temp.length == 2 && command.equalsIgnoreCase("J"))
+            {
+                registers[0] = temp[1];
+                Print.ln("command: " + command );
+                Print.ln("offset: " + registers[0]);
+            }
+            else if(temp.length == 3 && (command.equalsIgnoreCase("LD") || command.equalsIgnoreCase("SD")))
+            {
+                registers[0] = temp[1].split(",")[0];
+                registers[1] = temp[2].split("\\(")[0];
+                registers[2] = temp[2].split("\\(")[1].split("\\)")[0];
+                Print.ln("command: " + command );
+                Print.ln("rt: " + registers[0]);
+                Print.ln("offset: " + registers[1]);
+                Print.ln("base: " + registers[2]);
+            }
+            else if (temp.length == 4 && (command.equalsIgnoreCase("DADDIU") || command.equalsIgnoreCase("OR")
+                                       || command.equalsIgnoreCase("DSUBU") || command.equalsIgnoreCase("SLT")
+                                       || command.equalsIgnoreCase("BNE")))
+            {
+                registers[0] = temp[1].split(",")[0];
+                registers[1] = temp[2].split(",")[0];
+                registers[2] = temp[3];
+                Print.ln("command: " + command );
+                Print.ln("rd: " + registers[0]);
+                Print.ln("rs: " + registers[1]);
+                Print.ln("imm: " + registers[2]);
+            }
+            else
+            {
+                Print.ln("Syntax Error");
+                return null;
+            }
+            Instruction ins;
+            ins = formInstruction(i, singleLine, command, registers);
 
-			Instruction ins;
-			ins = formInstruction(i, singleLine, command, registers);
 
-
-			instructions.add(ins);
-//			/* checks if the line has content */
-//			if (scanner.hasNext()) {
-//				String firstArg = scanner.next();
-//				String[] registers;
-//				command = scanner.next();
+            instructions.add(ins);
+//          /* checks if the line has content */
+//          if (scanner.hasNext()) {
+//              String firstArg = scanner.next();
+//              String[] registers;
+//              command = scanner.next();
 //
-//				Instruction ins = null;
+//              Instruction ins = null;
 //
-//					ins = getInstruction(i, singleLine, command, registers);
-//					instructions.add(ins);
-//				scanner.close();
-//			}
-		}
+//                  ins = getInstruction(i, singleLine, command, registers);
+//                  instructions.add(ins);
+//              scanner.close();
+//          }
+        }
 
-		return instructions;
-	}
+        return instructions;
+    }
 
-	public Instruction formInstruction(int i, String singleLine, String command, String[] registers)
-	{
-		Instruction instruction;
-		String rd, rs, rt, imm;
+    public Instruction formInstruction(int i, String singleLine, String command, String[] registers)
+    {
+        Instruction instruction;
+        String rd, rs, rt, imm, offset, base;
 
-		rd  = registers[0];
-		rs  = registers[1];
-		rt  = registers[2];
-		imm = registers[2];
+        rd  = registers[0];
+        rs  = registers[1];
+        rt  = registers[2];
+        imm = registers[2];
+        offset = null;
+        base = null;
 
-		if( command.equals( "DADDIU" ) )
-		{
+        if( command.equalsIgnoreCase( "DADDIU" ) )
+        {
+            rt = rd;
+            rd = null;
+            if( imm.startsWith("#") )
+            {
+                imm = imm.substring(1);
+            }
+        }
+        else if( command.equalsIgnoreCase( "DSUBU" ) || command.equalsIgnoreCase( "OR" ) || command.equalsIgnoreCase( "SLT" ))
+        {
+            imm = null;
+        }
+        else if( command.equalsIgnoreCase( "LD" ) || command.equalsIgnoreCase( "SD" ) )
+        {
+            base = rt;
+            rt = rd;
+            rd = null;
+            offset = rs;
+            if( offset.startsWith("#") )
+            {
+                offset = offset.substring(1);
+            }
+            rs = null;
+            imm = null;
+        }
+        else if( command.equalsIgnoreCase( "BNE" ) )
+        {
+            offset = rt;
+            rt = rs;
+            rs = rd;
+            rd = null;
+            imm = null;
+        }
+        else if( command.equalsIgnoreCase( "J" ) )
+        {
+            offset = rd;
+            rd = null;
+            rs = null;
+            rt = null;
+            imm = null;
+        }
+        else if( command.equalsIgnoreCase( "NOP" ) )
+        {
+            rd = null;
+            rs = null;
+            rt = null;
+            imm = null;
+        }
+        else
+        {
+            Print.ln("Invalid Instruction");
+            return null;
+        }
 
-			if( imm.startsWith("#") )
-			{
-				imm = imm.substring(1);
-				rt = imm;
-			}
-		}
+        instruction = new Instruction(i, singleLine, command, null, rd, rs, rt, imm, offset, base);
 
-		instruction = new Instruction(i, singleLine, command, null, rd, rs, rt, imm, null, null);
-
-		return instruction;
-	}
+        return instruction;
+    }
 }
